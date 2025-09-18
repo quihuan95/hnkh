79252993c02fb0fbaf0a9b5067c9b7e0e8bcd3bf
hoabinhgroup <louis.standbyme@gmail.com>
2025-09-18 16:08:24 +0700

    Comment out action link for conditional rendering

--- resources/views/components/conference-sections.blade.php
+++ resources/views/components/conference-sections.blade.php
@@ -152,14 +152,14 @@
               <!-- Action Icons -->
               <div class="flex items-center gap-2">
                 @if (isset($item['type']))
-                <a target="_blank" href="{{ $item['url'] }}"
+                {{-- <a target="_blank" href="{{ $item['url'] }}"
                   class="p-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full transition-colors duration-200"
                   title="Mở liên kết">
                   <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
                   </svg>
-                </a>
+                </a> --}}
                 @endif
 
                 <!-- Dropdown Toggle Button (only show if has children) -->
